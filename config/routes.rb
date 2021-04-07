b34fb53bde96396cba62dc764e89c38be2e039c2
Martin Bettaglio <martin.bettaglio@gmail.com>
2021-04-07 11:49:57 +0200

    home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get '/trayectoria', to: 'pages#trayectoria'
+  get '/contacto', to: 'pages#contacto'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
