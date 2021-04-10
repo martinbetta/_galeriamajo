68821a6ac4dcd91bdcfd6a5aeaad9736d974a619
Martin Bettaglio <martin.bettaglio@gmail.com>
2021-04-10 21:22:56 +0200

    view pinturas

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get '/trayectoria', to: 'pages#trayectoria'
   get '/contacto', to: 'pages#contacto'
+  resources :pinturas, only:[:index, :new, :show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,61 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Pintura.destroy_all
+Libro.destroy_all
+Video.destroy_all
+Dibujo.destroy_all
+Grafica.destroy_all
+Foto.destroy_all
+
+require 'nokogiri'
+require 'open-uri'
+
+
+file = URI.open('https://res.cloudinary.com/djrxx3xla/image/upload/v1616679278/opus_XI_yfjmbc.jpg')
+pintura = Pintura.new(title: 'Opus XI', detail: "Técnica mixta sobre chapa de madera y hierro", category:"El silencio del vacío", size:"100 X 100 cm" ,year:"2020")
+pintura.photo.attach(io: file, filename: 'opus_XI_yfjmbc.jpg', content_type: 'image/png')
+pintura.save
+
+
+file = URI.open('https://res.cloudinary.com/djrxx3xla/image/upload/v1616679278/opus_VIII_gniaud.jpg')
+pintura = Pintura.new(title: 'Opus VIII', detail: "Técnica mixta sobre chapa de madera y hierro", category:"El silencio del vacío", size:"40 X 40 cm" ,year:"2020")
+pintura.photo.attach(io: file, filename: 'opus_VIII_gniaud.jpg', content_type: 'image/png')
+pintura.save
+
+file = URI.open('https://res.cloudinary.com/djrxx3xla/image/upload/v1616679278/opus_XII_zn4qgu.jpg')
+pintura = Pintura.new(title: 'Opus XII', detail: "Técnica mixta sobre chapa de madera y hierro", category:"El silencio del vacío", size:"130 x 130 cm" ,year:"2020")
+pintura.photo.attach(io: file, filename: 'opus_XII_zn4qgu.jpg', content_type: 'image/png')
+pintura.save
+
+file = URI.open('https://res.cloudinary.com/djrxx3xla/image/upload/v1616679278/opus_XIII_hrbtuj.jpg')
+pintura = Pintura.new(title: 'Opus XIII', detail: "Técnica mixta sobre chapa de madera y hierro", category:"El silencio del vacío", size:"130 x 130 cm" ,year:"2020")
+pintura.photo.attach(io: file, filename: 'opus_XIII_hrbtuj.jpg', content_type: 'image/png')
+pintura.save
+
+file = URI.open('https://res.cloudinary.com/djrxx3xla/image/upload/v1616679278/opus_XIV_a1lysf.jpg')
+pintura = Pintura.new(title: 'Opus XIV', detail: "Técnica mixta sobre chapa de madera y hierro", category:"El silencio del vacío", size:"100 X 100 cm" ,year:"2020")
+pintura.photo.attach(io: file, filename: 'opus_XIV_a1lysf.jpg', content_type: 'image/png')
+pintura.save
+
+file = URI.open('https://res.cloudinary.com/djrxx3xla/image/upload/v1616679278/opus_X_elkw8i.jpg')
+pintura = Pintura.new(title: 'Opus X', detail: "Técnica mixta sobre chapa de madera y hierro", category:"El silencio del vacío", size:"100 X 100 cm" ,year:"2020")
+pintura.photo.attach(io: file, filename: 'opus_X_elkw8i.jpg', content_type: 'image/png')
+pintura.save
+
+file = URI.open('https://res.cloudinary.com/djrxx3xla/image/upload/v1616679278/opus_XX_dmfoe2.jpg')
+pintura = Pintura.new(title: 'Opus XX', detail: "Técnica mixta sobre chapa de madera y hierro", category:"El silencio del vacío", size:"100 X 100 cm" ,year:"2020")
+pintura.photo.attach(io: file, filename: 'opus_XX_dmfoe2.jpg', content_type: 'image/png')
+pintura.save
+
+file = URI.open('https://res.cloudinary.com/djrxx3xla/image/upload/v1616679278/opus_VII_ullwtc.jpg')
+pintura = Pintura.new(title: 'Opus VII', detail: "Técnica mixta sobre chapa de madera y hierro", category:"El silencio del vacío", size:"40 X 40 cm" ,year:"2020")
+pintura.photo.attach(io: file, filename: 'opus_VII_ullwtc.jpg', content_type: 'image/png')
+pintura.save
+
+# file = URI.open('')
+# pintura = Pintura.new(title: 'Opus VIII', detail: "Técnica mixta sobre chapa de madera y hierro", category:"El silencio del vacío", size:"40 X 40 cm" ,year:"2020")
+# pintura.photo.attach(io: file, filename: 'opus_VIII_gniaud.jpg', content_type: 'image/png')
+# pintura.save
